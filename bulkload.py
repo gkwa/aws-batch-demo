17a6800c03680619743e475092b79276d9f03bc2
Taylor Monacelli <taylormonacelli@gmail.com>
2018-10-01 16:31:12 -0700

    Whitespace

--- bulkload.py
+++ bulkload.py
@@ -11,16 +11,14 @@ i = 0
 while (i < 10):
     j = 0
     while (j < 10):
-        print (i, j)
-        
+        print(i, j)
+
         table.put_item(
             Item={
-                'pk':i,
-                'sk':j,
-                'filler':{"S":filler}
+                'pk': i,
+                'sk': j,
+                'filler': {"S": filler}
             }
         )
         j += 1
     i += 1
-
-
